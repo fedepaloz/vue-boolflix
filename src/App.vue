535417349611f72e18b4840a3a30a23dfd3c0c49
fedepaloz <palozzifederico@gmail.com>
2022-08-21 15:41:50 +0200

    milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SearchBar placeholder="cerca film" @search="fetchMovies"></SearchBar>
+    <section id="movies">
+      <h2>movies</h2>
+      <ul v-for="movie in movies" :key="movie.id">
+        <li>{{movie.title}}</li>
+        <li>{{movie.original_title}}</li>
+        <li>{{movie.original_language}}</li>
+        <li>{{movie.vote_average}}</li>
+      </ul>
+    </section>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import SearchBar from "./components/SearchBar.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { SearchBar },
+  data() {
+    return {
+      movies: [],
+      api: {
+        language: "it",
+        key: "fc1383bffc985a009aa4698df58ddc3e",
+        baseUri: "https://api.themoviedb.org/3",
+      },
+    };
+  },
+  methods: {
+    fetchMovies(query) {
+      const { language, key, baseUri } = this.api;
+      const config = {
+        params: {
+          api_key: key,
+          language: language,
+          query: query,
+        },
+      };
+      axios.get(`${baseUri}/search/movie`, config).then((res) => {
+        this.movies = res.data.results;
+      });
+    },
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style>
+</style> 
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input type="text" :placeholder="placeholder" v-model.trim="term" @keyup.enter="emitSearch"> 
+    <button type="button" @click="emitSearch" >cerca</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'SearchBar',
+    props:{
+        placeholder: {
+            type:String,
+            required:false,
+            default: 'cerca'
+
+        },
+        buttonText:{
+            type:String,
+            required:false,
+            default:'cerca' 
+        }
+    },
+    data(){
+        return{
+            term:'',
+        }
+    },
+    methods:{
+        emitSearch(){
+            this.$emit('search',this.term);
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
